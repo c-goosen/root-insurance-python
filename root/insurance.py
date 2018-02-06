73f48ed3b928a8d4083a00d165512d7b88c80167
Brendan Ball <ball.brendan50@gmail.com>
2018-02-06 20:51:20 +0200

    Resources implemented: policies, quotes

--- root/insurance.py
+++ root/insurance.py
@@ -11,7 +11,9 @@ class Client:
         self.applications = Applications(self)
         self.claims = Claims(self)
         self.policyholders = PolicyHolders(self)
+        self.policies = Policies(self)
         self.gadgets = Gadgets(self)
+        self.quotes = Quotes(self)
 
     def call(self, method, path, params=None, **kwargs):
         resp = requests.request(method, f'{self.baseURL}/{path}', params=params, auth=(self.appID, self.appSecret))
@@ -34,10 +36,10 @@ class Applications(Resource):
     
     def create(self, policyholder_id, quote_package_id, monthly_premium, serial_number=None):
         data = {
-            policyholder_id:  policyholder_id,
-            quote_package_id: quote_package_id,
-            monthly_premium:  monthly_premium,
-            serial_number:    serial_number
+            "policyholder_id":  policyholder_id,
+            "quote_package_id": quote_package_id,
+            "monthly_premium":  monthly_premium,
+            "serial_number":    serial_number
         }
         return self.call("post", "applications", json=data)
 
@@ -61,20 +63,20 @@ class Claims(Resource):
 
     def open(self, policy_id=None, policy_holder_id=None):
         data = {
-            policy_id: policy_id,
-            policy_holder_id: policy_holder_id
+            "policy_id": policy_id,
+            "policy_holder_id": policy_holder_id
         }
         return self.call("post", "claims", json=data)
 
     def link_policy(self, claim_id, policy_id):
         data = {
-            policy_id: policy_id
+            "policy_id": policy_id
         }
         return self.call("post", f'claims/{claim_id}/policy', json=data)
 
     def link_policy_holder(self, claim_id, policy_holder_id):
         data = {
-            policy_holder_id: policy_holder_id
+            "policy_holder_id": policy_holder_id
         }
         return self.call("post", f'claims/{claim_id}/policyholder', json=data)
 
@@ -89,12 +91,12 @@ class PolicyHolders(Resource):
 
     def create(self, id, first_name, last_name, email=None, date_of_birth=None, cellphone=None):
         data = {
-            id:            id,
-            first_name:    first_name,
-            last_name:     last_name,
-            date_of_birth: date_of_birth,
-            email:         email,
-            cellphone:     cellphone
+            "id":            id,
+            "first_name":    first_name,
+            "last_name":     last_name,
+            "date_of_birth": date_of_birth,
+            "email":         email,
+            "cellphone":     cellphone
         }
         return self.call("post", "policyholders", json=data)
 
@@ -106,14 +108,102 @@ class PolicyHolders(Resource):
 
     def update(self, id, email=None, cellphone=None):
         data = {
-            email:     email,
-            cellphone: cellphone
+            "email":     email,
+            "cellphone": cellphone
         }
         return self.call("patch", f'policyholders/{id}', json=data)
 
     def list_events(self, id):
         return self.call("get", f'policyholders/{id}/events')
+
+
+class Policies(Resource):
+    def __init__(self, client):
+         super().__init__(client)    
+
+    def issue(self, application_id):
+        data = {
+            "application_id": application_id,
+        }
+        return self.call("post", "policies", json=data)
+
+    def add_beneficiary(self, policy_id, beneficiary_id, first_name, last_name, percentage):
+        data = {
+            "id":         beneficiary_id,
+            "first_name": first_name,
+            "last_name":  last_name,
+            "percentage": percentage
+        }
+        return self.call("put", f'policies/{policy_id}/beneficiaries', json=data)
         
+    def list(self):
+        return self.call("get", "policies")
+
+    def get(self, policy_id):
+        return self.call("get", f'policies/{policy_id}')
+
+    def cancel(self, policy_id, reason):
+        data = {"reason": reason}
+        return self.call("post", f'policies/{policy_id}/cancel', json=data)
+        
+    def replace(self, policy_id, quote_package_id):
+        data = {"quote_package_id": quote_package_id}
+        return self.call("post", f'policies/{policy_id}/replace', json=data)
+
+    def update_billing_amount(self, policy_id, billing_amount):
+        data = {"billing_amount": billing_amount}
+        return self.call("post", f'policies/{policy_id}/billing_amount', json=data)
+
+    def list_beneficiaries(self, policy_id):
+        return self.call("get", f'policies/{policy_id}/beneficiaries')
+
+    def list_events(self, policy_id):
+        return self.call("get", f'policies/{policy_id}/events')
+
+
+class Quotes(Resource):
+    def __init__(self, client):
+         super().__init__(client)
+
+    def create(self, type_, opts):
+        data = {}
+        if type_ == "root_gadgets":
+            data = self._gadget_quote(opts)
+        elif type_ == "root_term":
+            data = self._term_quote(opts)
+        elif type_ == "root_funeral":
+            data = self._funeral_quote(opts)
+        else:
+            raise Exception("invalid quote type")
+        self.call("post", "quotes", json=data)
+
+    def _gadget_quote(self, opts):
+        return {
+            "type": "root_gadgets",
+            "model_name": opts["model_name"]
+        }
+
+    def _term_quote(self, opts):
+        return {
+            "type":             "root_term",
+            "cover_amount":     opts["cover_amount"],
+            "cover_period":     opts["cover_period"],
+            "education_status": opts["education_status"],
+            "smoker":           opts["smoker"],
+            "gender":           opts["gender"],
+            "age":              opts["age"],
+            "basic_income_per_month": opts["basic_income_per_month"],
+        }
+
+    def _funeral_quote(self, opts):
+        return {
+            "type":                 "root_funeral",
+            "cover_amount":         opts["cover_amount"],
+            "has_spouse":           opts["has_spouse"],
+            "number_of_children":   opts["number_of_children"],
+            "extended_family_ages": opts["extended_family_ages"]
+        }
+
 
 class Gadgets(Resource):
     def __init__(self, client):
